dc3165167ad015f9fca61cfe09fa041c94f9b38b
James Cox <16992818+jcox250@users.noreply.github.com>
2021-04-03 21:05:44 +0100

    init

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/jcox250/log
+
+go 1.16
+
+require github.com/go-kit/kit v0.10.0
---
+++ log.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"io"
+
+	"github.com/go-kit/kit/log"
+	"github.com/go-kit/kit/log/level"
+)
+
+type LeveledLogger interface {
+	Info(keyvals ...interface{})
+	Debug(keyvals ...interface{})
+	Error(keyvals ...interface{})
+}
+
+type logger struct {
+	log.Logger
+	debug bool
+}
+
+func NewLeveledLogger(w io.Writer, debug bool) LeveledLogger {
+	w = log.NewSyncWriter(w)
+	kitlogger := log.NewLogfmtLogger(w)
+	level.NewFilter(kitlogger, level.AllowAll())
+	return &logger{kitlogger, debug}
+}
+
+func (l *logger) Info(keyvals ...interface{}) {
+	level.Info(l).Log(keyvals...)
+}
+func (l *logger) Debug(keyvals ...interface{}) {
+	if !l.debug {
+		return
+	}
+	level.Debug(l).Log(keyvals...)
+}
+func (l *logger) Error(keyvals ...interface{}) {
+	level.Error(l).Log(keyvals...)
+}
